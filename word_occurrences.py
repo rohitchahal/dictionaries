78d9bbdea9ca1081ccbae8c0e8f4058da2f4a217
rohitchahal <rohit.chahal@my.jcu.edu.au>
2016-04-15 11:13:03 +1000

    final commit

---
+++ word_occurrences.py
@@ -0,0 +1,13 @@
+sentence = input("enter any sentence:")
+frequency_words = {}
+words = sentence.split()
+for word in words:
+    if word in frequency_words:
+        frequency_words[word] += 1
+    else:
+        frequency_words[word] = 1
+print("Text : ", sentence)
+for word in sorted(frequency_words):
+    print("{:<} : {} ".format(word, frequency_words[word]))
+
+
